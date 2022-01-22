ca7798acc214b026db34d31b1f3f5e098bd58a4b
FarukErat <92527106+FarukErat@users.noreply.github.com>
2022-01-23 02:03:55 +0300

    Update doubly_linked_list.cpp

--- doubly_linked_list.cpp
+++ doubly_linked_list.cpp
@@ -1,44 +1,12 @@
 #include <iostream>
+#include "DLL.h"
 
 using namespace std;
 
-typedef struct node
-{
-    int value;
-    struct node *next;
-    struct node *previous;
-} Node;
-
-typedef struct list
-{
-    int memberNumber;
-    Node *firstNode;
-    Node *lastNode;
-} List;
-
-enum howToPrint
-{
-    FORWARD = false,
-    BACKWARD = true
-};
-
-class doubly_linked
-{
-public:
-    List list;
-    doubly_linked();
-    ~doubly_linked();
-    void printMenu();
-    void append(int newElement);
-    void pop(int index);
-    void printList(bool isBackward = FORWARD);
-};
-
 int main()
 {
-    int value;
-    unsigned int choice, index;
-    doubly_linked l;
+    DLList<int> l;
+    int choice, data, index;
 
     cout << "----------" << endl
          << "  [Menu]   " << endl
@@ -46,7 +14,17 @@ int main()
 
     while (true)
     {
-        l.printMenu();
+        cout << endl
+             << endl
+             << "1. add a value" << endl
+             << "2. remove a value" << endl
+             << "3. change a value" << endl
+             << "4. insert a value" << endl
+             << "5. print the list forward" << endl
+             << "6. print the list backward" << endl
+             << "0. EXIT" << endl
+             << "Enter your choice: ";
+
         cin >> choice;
         switch (choice)
         {
@@ -54,19 +32,33 @@ int main()
             return 0;
         case 1:
             cout << "Enter a number to append: ";
-            cin >> value;
-            l.append(value);
+            cin >> data;
+            l.append(data);
             break;
         case 2:
-            cout << "Enter the index to pop an element: ";
+            cout << "Enter an index to pop an element: ";
             cin >> index;
             l.pop(index);
             break;
         case 3:
-            l.printList(FORWARD);
+            cout << "Enter an index to change a value: ";
+            cin >> index;
+            cout << "Enter the new value: ";
+            cin >> data;
+            l.change(index, data);
             break;
         case 4:
-            l.printList(BACKWARD);
+            cout << "Enter an index to insert a value: ";
+            cin >> index;
+            cout << "Enter a new value: ";
+            cin >> data;
+            l.insert(index, data);
+            break;
+        case 5:
+            l.Display();
+            break;
+        case 6:
+            l.RevDisplay();
             break;
         default:
             cout << "Invalid choice!" << endl
@@ -75,147 +67,5 @@ int main()
         }
     }
 
-    return EXIT_SUCCESS;
-}
-
-doubly_linked::doubly_linked()
-{
-    this->list.firstNode = new Node;
-    this->list.firstNode->previous = NULL;
-    this->list.firstNode->next = NULL;
-    this->list.lastNode = this->list.firstNode;
-    this->list.memberNumber = 0;
-}
-
-doubly_linked::~doubly_linked()
-{
-    Node *ptr = this->list.firstNode;
-    Node *nodeToRemove;
-
-    while (ptr->next != NULL)
-    {
-        nodeToRemove = ptr;
-        ptr = ptr->next;
-        delete nodeToRemove;
-    }
-    delete ptr;
-    cout << endl
-         << "The program exitted";
-}
-
-void doubly_linked::printMenu()
-{
-    cout << endl
-         << endl
-         << "1. add a value" << endl
-         << "2. remove a value" << endl
-         << "3. print the list forward" << endl
-         << "4. print the list backward" << endl
-         << "0. EXIT" << endl
-         << "Enter your choice: ";
-}
-
-void doubly_linked::append(int newElement)
-{
-    if (this->list.memberNumber == 0)
-    {
-        this->list.firstNode->value = newElement;
-        this->list.memberNumber++;
-        return;
-    }
-
-    Node *newNode = new Node;
-    newNode->value = newElement;
-
-    this->list.lastNode->next = newNode;
-    newNode->previous = this->list.lastNode;
-    this->list.lastNode = newNode;
-    this->list.lastNode->next = NULL;
-    this->list.memberNumber++;
-}
-
-void doubly_linked::pop(int index)
-{
-
-    if (this->list.memberNumber == 0 || index > this->list.memberNumber)
-    {
-        cout << "Invalid value!";
-        return;
-    }
-
-    if (this->list.memberNumber == 1)
-    {
-        this->list.memberNumber--;
-        cout << "The number has been removed";
-        return;
-    }
-
-    Node *ptr = NULL;
-
-    if (index == 1)
-    {
-        ptr = this->list.firstNode;
-        this->list.firstNode = this->list.firstNode->next;
-        this->list.firstNode->previous = NULL;
-        this->list.memberNumber--;
-        delete ptr;
-        return;
-    }
-
-    if (index == this->list.memberNumber)
-    {
-        ptr = this->list.lastNode;
-        this->list.lastNode = this->list.lastNode->previous;
-        this->list.lastNode->next = NULL;
-        this->list.memberNumber--;
-        delete ptr;
-        return;
-    }
-
-    int i = 1;
-    ptr = this->list.firstNode;
-    while (i < index - 1 && ptr->next != NULL)
-    {
-        ptr = ptr->next;
-        i++;
-    }
-
-    Node *nodeToRemove = ptr->next;
-    nodeToRemove->previous->next = nodeToRemove->next;
-    nodeToRemove->next->previous = nodeToRemove->previous;
-    delete nodeToRemove;
-    this->list.memberNumber--;
-    cout << endl
-         << "The number has been removed";
-}
-
-void doubly_linked::printList(bool isBackward)
-{
-    if (this->list.memberNumber == 0)
-    {
-        cout << "The list is empty!";
-        return;
-    }
-
-    Node *ptr;
-    if (isBackward)
-    {
-        ptr = this->list.lastNode;
-        cout << ptr->value << endl;
-        while (ptr->previous != NULL)
-        {
-            ptr = ptr->previous;
-            cout << ptr->value << endl;
-        }
-    }
-    else
-    {
-        ptr = this->list.firstNode;
-        cout << ptr->value << endl;
-        while (ptr->next != NULL)
-        {
-            ptr = ptr->next;
-            cout << ptr->value << endl;
-        }
-    }
+    return 0;
 }
