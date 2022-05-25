479672e693357ae549b6155ebdc35ed0cbcb7528
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-05-25 19:27:55 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,8 +3,24 @@
 
 using namespace std;
 
+void first(int num1, int num2)
+{
+    cout << "First function called with " << num1 << " and " << num2 << endl;
+}
+
+void second(int num1, int num2)
+{
+    cout << "Second function called with " << num1 << " and " << num2 << endl;
+}
+
+void third(int num1, int num2)
+{
+    cout << "Third function called with " << num1 << " and " << num2 << endl;
+}
+
 int main()
 {
+    // test on queues
     Queue<string> q;
     q.push("Hello");                                // Hello
     q.push("World");                                // Hello World
@@ -15,6 +31,7 @@ int main()
     q.pop();                                        // Universe
     cout << q.get(0) << " " << q.getSize() << endl; // Universe(0)
 
+    // test on stacks
     Stack<string> s;
     s.push("Hello");                                // Hello
     s.push("World");                                // Hello World
@@ -24,15 +41,27 @@ int main()
     s.pop();                                        // Hello World
     cout << s.get(0) << " " << s.getSize() << endl; // Hello(0) World(1)
 
+    // test on lists
     DLList<string> l;
-    l.append("Hello");                              // Hello
-    l.append("World");                              // Hello World
-    l.append("and");                                // Hello World and
-    l.append("Universe");                           // Hello World and Universe
-    l.pop();                                        // Hello World and
-    l.change(1, "Mars");                            // Hello Mars and
-    l.pop(1);                                       // Hello and
-    cout << l.get(1) << " " << l.getSize() << endl; // Hello(0) and(1)
+    l.append("Hello");                           // Hello
+    l.append("World");                           // Hello World
+    l.append("and");                             // Hello World and
+    l.append("Universe");                        // Hello World and Universe
+    l.pop();                                     // Hello World and
+    l.change(1, "Mars");                         // Hello Mars and
+    l.pop(1);                                    // Hello and
+    cout << l.get(1) << " " << l.Size() << endl; // Hello(0) and(1)
+
+    // test on function lists
+    DLList<void (*)(int, int)> funcs;
+
+    funcs.append(first);
+    funcs.append(second);
+    funcs.append(third);
+
+    funcs.get(1)(0, 5); // call second function with 0 and 5
+    funcs.get(0)(3, 1); // call first function with 3 and 1
+    funcs.get(2)(7, 2); // call third function with 7 and 5
 
     return 0;
 }
