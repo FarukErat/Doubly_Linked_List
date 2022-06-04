9295e2701ce5adca3099ebd1dd4c9d7991a65f03
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-06-04 18:25:03 +0300

    Update test3.cpp

--- test3.cpp
+++ test3.cpp
@@ -5,15 +5,24 @@ using namespace std;
 
 int main()
 {
+    // list of ints
     DLList<int> list1;
-    list1.append(1);
-    list1.append(2);
-    list1.append(3);
+    list1.append(456);
+
+    // list of pointers to lists of ints
     DLList<DLList<int> *> list2;
     list2.append(&list1);
+
+    // list pointers to lists of pointers to lists of ints
     DLList<DLList<DLList<int> *> *> list3;
     list3.append(&list2);
-    cout << list3.get(0)->get(0)->get(0) << endl;
+
+    cout << *list3[0][0][0];
+    /* prints 1
+    list3[0] returns a pointer to a list of pointers to lists of ints
+    list3[0][0] returns a pointer to a list of ints
+    list3[0][0][0] returns an int
+    */
 
     return 0;
 }
