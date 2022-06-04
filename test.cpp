c3da203a01d287a580207bb5db3b6531fab9d95f
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-06-04 17:56:22 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -22,38 +22,38 @@ int main()
 {
     // test on queues
     Queue<string> q;
-    q.push("Hello");                             // Hello
-    q.push("World");                             // Hello World
-    q.push("and");                               // Hello World and
-    q.push("Universe");                          // Hello World and Universe
-    q.pop();                                     // World and Universe
-    q.pop();                                     // and Universe
-    q.pop();                                     // Universe
-    cout << q.get(0) << " " << q.Size() << endl; // Universe(0)
-    cout << q;                                   // Universe
+    q.push("Hello");                         // Hello
+    q.push("World");                         // Hello World
+    q.push("and");                           // Hello World and
+    q.push("Universe");                      // Hello World and Universe
+    q.pop();                                 // World and Universe
+    q.pop();                                 // and Universe
+    q.pop();                                 // Universe
+    cout << q[0] << " " << q.Size() << endl; // Universe(0)
+    cout << q;                               // Universe
 
     // test on stacks
     Stack<string> s;
-    s.push("Hello");                             // Hello
-    s.push("World");                             // Hello World
-    s.push("and");                               // Hello World and
-    s.push("Universe");                          // Hello World and Universe
-    s.pop();                                     // Hello World and
-    s.pop();                                     // Hello World
-    cout << s.get(0) << " " << s.Size() << endl; // Hello(0) World(1)
-    cout << s;                                   // Hello World
+    s.push("Hello");                         // Hello
+    s.push("World");                         // Hello World
+    s.push("and");                           // Hello World and
+    s.push("Universe");                      // Hello World and Universe
+    s.pop();                                 // Hello World and
+    s.pop();                                 // Hello World
+    cout << s[0] << " " << s.Size() << endl; // Hello(0) World(1)
+    cout << s;                               // Hello World
 
     // test on lists
     DLList<string> l;
-    l.append("Hello");                           // Hello
-    l.append("World");                           // Hello World
-    l.append("and");                             // Hello World and
-    l.append("Universe");                        // Hello World and Universe
-    l.pop();                                     // Hello World and
-    l.change(1, "Mars");                         // Hello Mars and
-    l.pop(1);                                    // Hello and
-    cout << l.get(1) << " " << l.Size() << endl; // Hello(0) and(1)
-    cout << l;                                   // Hello and
+    l.append("Hello");                       // Hello
+    l.append("World");                       // Hello World
+    l.append("and");                         // Hello World and
+    l.append("Universe");                    // Hello World and Universe
+    l.pop();                                 // Hello World and
+    l.change(1, "Mars");                     // Hello Mars and
+    l.pop(1);                                // Hello and
+    cout << l[0] << " " << l.Size() << endl; // Hello(0) and(1)
+    cout << l;                               // Hello and
 
     // test on function lists
     DLList<void (*)(int, int)> funcs;
@@ -62,9 +62,9 @@ int main()
     funcs.append(second);
     funcs.append(third);
 
-    funcs.get(1)(0, 5); // call second function with 0 and 5
-    funcs.get(0)(3, 1); // call first function with 3 and 1
-    funcs.get(2)(7, 2); // call third function with 7 and 5
+    funcs[1](0, 5); // call second function with 0 and 5
+    funcs[0](3, 1); // call first function with 3 and 1
+    funcs[2](7, 2); // call third function with 7 and 5
     cout << funcs;
 
     return 0;
