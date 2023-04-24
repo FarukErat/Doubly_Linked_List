23ffae72030b3622531cbd22124f1f3e0818de95
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-04-24 21:21:32 +0300

    Update DLList.hpp

--- DLList.hpp
+++ DLList.hpp
@@ -4,27 +4,30 @@
 
 #include <iostream>
 
+using std::initializer_list;
+using std::ostream;
+using std::istream;
+using std::out_of_range;
+
 template <typename T>
-class Node
-{
+class Node {
 public:
     T data;
-    Node<T> *next = nullptr;
-    Node<T> *prev = nullptr;
+    Node<T>* next = nullptr;
+    Node<T>* prev = nullptr;
 };
 template <typename T>
-class DLList
-{
+class DLList {
 private:
     int size = 0;
-    Node<T> *head = nullptr;
-    Node<T> *tail = nullptr;
+    Node<T>* head = nullptr;
+    Node<T>* tail = nullptr;
     bool boundCheck(int index);
 
 public:
     T input;
-    DLList(){};
-    DLList(std::initializer_list<T> const l);
+    DLList() {};
+    DLList(initializer_list<T> const l);
     ~DLList();
 
     int getSize() const { return size; }; /* read-only */
@@ -35,29 +38,26 @@ public:
 
     T pop();
 
-    T &operator[](int index);
+    T& operator[](int index);
 
-    void operator=(DLList<T> &list);
-    void operator=(std::initializer_list<T> const l);
+    void operator=(DLList<T>& list);
+    void operator=(initializer_list<T> const l);
 
     void operator+=(T data);
-    void operator+=(DLList<T> &list);
-    void operator+=(std::initializer_list<T> const l);
+    void operator+=(DLList<T>& list);
+    void operator+=(initializer_list<T> const l);
 
     template <typename U>
-    friend std::ostream &operator<<(std::ostream &out, DLList<U> &list);
+    friend ostream& operator<<(ostream& out, DLList<U>& list);
     template <typename U>
-    friend std::istream &operator>>(std::istream &in, DLList<U> &list);
+    friend istream& operator>>(istream& in, DLList<U>& list);
 };
 
 template <typename T>
-bool DLList<T>::boundCheck(int index)
-{
-    Node<T> *iter = head;
-    for (int i = 0; i < index; i++)
-    {
-        if (iter == nullptr)
-        {
+bool DLList<T>::boundCheck(int index) {
+    Node<T>* iter = head;
+    for (int i = 0; i < index; i++) {
+        if (iter == nullptr) {
             return false;
         }
         iter = iter->next;
@@ -66,32 +66,26 @@ bool DLList<T>::boundCheck(int index)
 };
 
 template <typename T>
-DLList<T>::DLList(std::initializer_list<T> const l)
-{
-    for (auto i : l)
-    {
+DLList<T>::DLList(initializer_list<T> const l) {
+    for (auto i : l) {
         this->append(i);
     }
 };
 template <typename T>
-DLList<T>::~DLList()
-{
-    Node<T> *ptr = head;
-    while (ptr != nullptr)
-    {
-        Node<T> *temp = ptr;
+DLList<T>::~DLList() {
+    Node<T>* ptr = head;
+    while (ptr != nullptr) {
+        Node<T>* temp = ptr;
         ptr = ptr->next;
         delete temp;
     }
 };
 
 template <typename T>
-void DLList<T>::insert(T data, int index)
-{
-    Node<T> *newNode = new Node<T>;
+void DLList<T>::insert(T data, int index) {
+    Node<T>* newNode = new Node<T>;
     newNode->data = data;
-    if (head == nullptr)
-    {
+    if (head == nullptr) {
         newNode->next = nullptr;
         newNode->prev = nullptr;
         head = newNode;
@@ -99,8 +93,7 @@ void DLList<T>::insert(T data, int index)
         size++;
         return;
     }
-    if (index == 0)
-    {
+    if (index == 0) {
         newNode->next = head;
         newNode->prev = nullptr;
         head->prev = newNode;
@@ -108,8 +101,7 @@ void DLList<T>::insert(T data, int index)
         size++;
         return;
     }
-    else if (index == size)
-    {
+    else if (index == size) {
         newNode->prev = tail;
         newNode->next = nullptr;
         tail->next = newNode;
@@ -117,24 +109,19 @@ void DLList<T>::insert(T data, int index)
         size++;
         return;
     }
-    else
-    {
+    else {
         newNode->next = nullptr;
         newNode->prev = nullptr;
-        Node<T> *ptr = nullptr;
-        if (index < size / 2)
-        {
+        Node<T>* ptr = nullptr;
+        if (index < size / 2) {
             ptr = head;
-            for (int i = 0; i < index; i++)
-            {
+            for (int i = 0; i < index; i++) {
                 ptr = ptr->next;
             }
         }
-        else
-        {
+        else {
             ptr = tail;
-            for (int i = size - 1; i > index; i--)
-            {
+            for (int i = size - 1; i > index; i--) {
                 ptr = ptr->prev;
             }
         }
@@ -146,52 +133,42 @@ void DLList<T>::insert(T data, int index)
     }
 };
 template <typename T>
-void DLList<T>::append(T data)
-{
+void DLList<T>::append(T data) {
     insert(data, size);
 }
 template <typename T>
-void DLList<T>::remove(int index)
-{
+void DLList<T>::remove(int index) {
     if (!boundCheck(index))
         return;
-    Node<T> *ptr;
-    if (size == 1)
-    {
+    Node<T>* ptr;
+    if (size == 1) {
         delete head;
         head = nullptr;
         tail = nullptr;
         size--;
         return;
     }
-    if (index < size / 2)
-    {
+    if (index < size / 2) {
         ptr = head;
-        for (int i = 0; i < index; i++)
-        {
+        for (int i = 0; i < index; i++) {
             ptr = ptr->next;
         }
     }
-    else
-    {
+    else {
         ptr = tail;
-        for (int i = size - 1; i > index; i--)
-        {
+        for (int i = size - 1; i > index; i--) {
             ptr = ptr->prev;
         }
     }
-    if (ptr->prev == nullptr)
-    {
+    if (ptr->prev == nullptr) {
         head = ptr->next;
         head->prev = nullptr;
     }
-    if (ptr->next == nullptr)
-    {
+    if (ptr->next == nullptr) {
         tail = ptr->prev;
         tail->next = nullptr;
     }
-    if (ptr->prev != nullptr && ptr->next != nullptr)
-    {
+    if (ptr->prev != nullptr && ptr->next != nullptr) {
         ptr->prev->next = ptr->next;
         ptr->next->prev = ptr->prev;
     }
@@ -200,8 +177,7 @@ void DLList<T>::remove(int index)
 };
 
 template <typename T>
-T DLList<T>::pop()
-{
+T DLList<T>::pop() {
     T data = tail->data;
     remove(size - 1);
     return data;
@@ -209,26 +185,20 @@ T DLList<T>::pop()
 
 /* I N D E X */
 template <typename T>
-T &DLList<T>::operator[](int index)
-{
-    if (index >= size || boundCheck(index) == false)
-    {
-        throw std::out_of_range("Index out of bound.");
+T& DLList<T>::operator[](int index) {
+    if (index >= size || boundCheck(index) == false) {
+        throw out_of_range("Index out of bound.");
     }
-    Node<T> *ptr;
-    if (index < size / 2)
-    {
+    Node<T>* ptr;
+    if (index < size / 2) {
         ptr = head;
-        for (int i = 0; i < index; i++)
-        {
+        for (int i = 0; i < index; i++) {
             ptr = ptr->next;
         }
     }
-    else
-    {
+    else {
         ptr = tail;
-        for (int i = size - 1; i > index; i--)
-        {
+        for (int i = size - 1; i > index; i--) {
             ptr = ptr->prev;
         }
     }
@@ -237,57 +207,46 @@ T &DLList<T>::operator[](int index)
 
 /* A S S I G N M E N T */
 template <typename T>
-void DLList<T>::operator=(DLList<T> &list)
-{
+void DLList<T>::operator=(DLList<T>& list) {
     if (this == &list)
         return;
     this->~DLList();
-    DLList<T> *temp = new (this) DLList<T>(); /* implicitly reconstructing */
-    for (int i = 0; i < list.getSize(); i++)
-    {
+    DLList<T>* temp = new (this) DLList<T>(); /* implicitly reconstructing */
+    for (int i = 0; i < list.getSize(); i++) {
         this->append(list[i]);
     }
 };
 template <typename T>
-void DLList<T>::operator=(std::initializer_list<T> const l)
-{
+void DLList<T>::operator=(initializer_list<T> const l) {
     this->~DLList();
-    DLList<T> *temp = new (this) DLList<T>();
-    for (auto i : l)
-    {
+    DLList<T>* temp = new (this) DLList<T>();
+    for (auto i : l) {
         this->append(i);
     }
 };
 
 /* P L U S   E Q U A L S */
 template <typename T>
-void DLList<T>::operator+=(T data)
-{
+void DLList<T>::operator+=(T data) {
     this->append(data);
 };
 template <typename T>
-void DLList<T>::operator+=(DLList<T> &list)
-{
-    for (int i = 0; i < list.getSize(); i++)
-    {
+void DLList<T>::operator+=(DLList<T>& list) {
+    for (int i = 0; i < list.getSize(); i++) {
         this->append(list[i]);
     }
 };
 template <typename T>
-void DLList<T>::operator+=(std::initializer_list<T> const l)
-{
-    for (auto i : l)
-    {
+void DLList<T>::operator+=(initializer_list<T> const l) {
+    for (auto i : l) {
         this->append(i);
     }
 };
 
 /* L E F T   S H I F T */
 template <typename T>
-std::ostream &operator<<(std::ostream &out, DLList<T> &list)
-{
-    for (int i = 0; i < list.getSize(); i++)
-    {
+ostream& operator<<(ostream& out, DLList<T>& list) {
+    for (int i = 0; i < list.getSize(); i++) {
         out << list[i] << ' ';
     }
     return out;
@@ -295,8 +254,7 @@ std::ostream &operator<<(std::ostream &out, DLList<T> &list)
 
 /* R I G H T   S H I F T */
 template <typename T>
-std::istream &operator>>(std::istream &in, DLList<T> &list)
-{
+istream& operator>>(istream& in, DLList<T>& list) {
     T data;
     in >> data;
     list.append(data);
